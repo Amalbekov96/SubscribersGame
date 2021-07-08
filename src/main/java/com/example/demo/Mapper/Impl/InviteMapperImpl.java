15095daeb738a034159ddb213b099996c15fc604
Amalbekov96 <amalbekov312@gmail.com>
2021-07-08 00:47:10 -0500

    Small changes in Mapper.

--- src/main/java/com/example/demo/Mapper/Impl/InviteMapperImpl.java
+++ src/main/java/com/example/demo/Mapper/Impl/InviteMapperImpl.java
@@ -1,6 +1,19 @@
 package com.example.demo.Mapper.Impl;
 
+import com.example.demo.Entity.Dto.InviteDto;
+import com.example.demo.Entity.Invites;
 import com.example.demo.Mapper.InviterMapper;
 
 public class InviteMapperImpl implements InviterMapper {
+    @Override
+    public Invites toInvites(InviteDto inviteDto) {
+
+        return null;
+    }
+
+    @Override
+    public InviteDto toInviteDto(Invites invite) {
+
+        return null;
+    }
 }
--- src/main/java/com/example/demo/Mapper/InviterMapper.java
+++ src/main/java/com/example/demo/Mapper/InviterMapper.java
@@ -1,9 +1,12 @@
 package com.example.demo.Mapper;
 
+import com.example.demo.Entity.Dto.InviteDto;
+import com.example.demo.Entity.Invites;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface InviterMapper {
-
+    Invites toInvites(InviteDto inviteDto);
+    InviteDto toInviteDto(Invites invite);
 
 }
